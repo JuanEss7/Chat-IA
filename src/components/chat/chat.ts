0a7ec8e8f9bc3005acdb9f5f853841d03e02c540
Juan <juanplazas28pa@gmail.com>
2024-08-06 17:20:35 -0500

    Refactorizando el archivo chat.ts

--- src/components/chat/chat.ts
+++ src/components/chat/chat.ts
@@ -1,5 +1,7 @@
-import { CreateWebWorkerMLCEngine, WebWorkerMLCEngine, type ChatCompletionMessageParam } from "@mlc-ai/web-llm";
-import validateMessage from "../../utils/validateMessage.ts";
+import { CreateWebWorkerMLCEngine, WebWorkerMLCEngine } from "@mlc-ai/web-llm";
+import type { ModelSelectedUser, Role, User, MessagesInterface } from "../../types/index.ts";
+import { ModelsIA } from "../../types/index.ts";
+import { getMessage, saveMessage, validateMessage } from "../../utils/index.ts";
 const container_messages = document.querySelector(
     ".container_messages",
 ) as HTMLUListElement;
@@ -14,118 +16,47 @@ const gpu_info = document.querySelector(".gpu_info") as HTMLSpanElement;
 const form_select_model = document.querySelector(
     ".form_model_select",
 ) as HTMLFormElement;
-const modelsIA = {
-    smol: "SmolLM-135M-Instruct-q4f32_1-MLC",
-    llama: "Llama-3.1-8B-Instruct-q4f32_1-MLC-1k",
-    hermes: "Hermes-2-Pro-Llama-3-8B-q4f16_1-MLC",
-    phi: "phi-2-q4f16_1-MLC",
-    gemma: "gemma-2b-it-q4f32_1-MLC",
-    gemma_2: "gemma-2b-it-q4f16_1-MLC",
-    tiny: "TinyLlama-1.1B-Chat-v0.4-q4f32_1-MLC",
-};
-const worker = new Worker(new URL("./worker.ts", import.meta.url), { type: 'module' })
 let engine: WebWorkerMLCEngine;
+let selectedModel: ModelsIA;
+let model_selected: ModelSelectedUser;
+let userInfo: User;
+const worker = new Worker(new URL("./worker.ts", import.meta.url), { type: 'module' })
+let messages: MessagesInterface[] = [];
+
 worker.addEventListener('error', (msg) => {
-    console.log(msg)
+    console.log('Ocurri un error en el worker', msg)
 })
-let selectedModel: string;
-let userInfo: any;
-document.addEventListener('DOMContentLoaded', async () => {
-    const resp = await fetch('/api/getUser');
-    userInfo = await resp.json();
-})
-form_select_model.addEventListener("submit", async (e) => {
+
+async function SelectModelEngine(e: SubmitEvent) {
     e.preventDefault();
-    const model_selected = new FormData(form_select_model).get(
+    const value_selected = new FormData(form_select_model).get(
         "model_selector",
-    ) as "llama" | "phi" | "gemma" | "hermes" | "tiny" | "smol";
-    if (!modelsIA[model_selected]) {
+    );
+    model_selected = value_selected as ModelSelectedUser;
+    if (!ModelsIA[model_selected]) {
         alert("El modelo que estas seleccionando no existe");
         return;
     }
-    selectedModel = modelsIA[model_selected] ?? modelsIA["smol"];
-    const newEngine = await createEngine(selectedModel);
+    selectedModel = ModelsIA[model_selected] ?? ModelsIA['smol'];
+    const newEngine = await CreateEngine(selectedModel);
     engine = newEngine!;
-    console.log({ engine, selectedModel })
-});
-interface MessagesInterface {
-    role: 'user' | 'assistant',
-    content: string,
-    tool_call_id?: string
 }
-let messages: MessagesInterface[][] = [];
-
-
-
-
-form?.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    errorMessage.style.display = "none";
-    errorMessage.textContent = "";
-    if (!selectedModel) {
-        alert('Por favor selecciona modelo antes de enviar un mensaje.')
-        return
-    }
-    //Validando mensaje de usuario
-    let message = input.value.trim();
-    const messageValdated = validateMessage(message);
-    if (messageValdated.error) {
-        errorMessage.style.display = "block";
-        errorMessage.textContent = messageValdated.message!;
-        return;
-    }
-
-    errorMessage.style.display = "none";
-    errorMessage.textContent = "";
-
-    //Agregando mensaje de usuario
-    const userMessage: MessagesInterface = {
-        role: 'user',
-        content: message,
-    };
-    addMessage("user", message);
-    input.value = "";
-    button.setAttribute('disabled', 'true')
-    //Agregando mensaje de IA
-    console.log(engine)
-    let reply = "";
-    // const response = await engine.chat.completions.create({
-    //     messages,
-    //     temperature: 1,
-    //     stream: true,
-    // });
-
-    // const textParagraph = addMessage("assistant", "");
-    // for await (const chunk of response) {
-    //     const [choice] = chunk.choices;
-    //     //choice?.delta? ---->  {role:'assistand', content:''}
-    //     const content = choice?.delta?.content ?? "";
-    //     reply += content;
-    //     textParagraph.textContent = reply;
-    // }
-    // const botMessage: MessagesInterface = {
-    //     role: "assistant",
-    //     content: reply,
-    // };
-    //SIN APLICAR EL STREAM, es decir, me devuelve el contenido una vez se haya completado la repuesta de la IA
-    // //response.choices[0].message ----> {role:'assistand', content:''}
-    // const botMessage = response.choices[0].message;
-    // if (!botMessage.content) {
-    // 	return;
-    // }
-    //addMessage(botMessage.role,botMessage.content);
-    // messages.push(botMessage);
-    messages.push([{ role: 'assistant', content: 'hola' }, userMessage]);
-    button.removeAttribute("disabled");
-    container_messages.scrollTop = container_messages.scrollHeight;
-    // await addMessageToDb(messages, selectedModel);
-    // if(!res.ok){
-    //     const
-    // }
-});
-
-//TODO:EL PARAM SEA UN OBJ, TIPARLO
-async function addMessage(role: "assistant" | "user", message: string) {
+async function DOMloaded() {
+    const resp = await fetch('/api/getUser');
+    if (!resp.ok) return
+    userInfo = await resp.json();
+    const infoStorage = getMessage(userInfo.email);
+    if (!infoStorage) return
+    const { messagesLocal = [], model = 'tiny' } = infoStorage;
+    const value_selected = document.getElementById('model_selector') as HTMLSelectElement;
+    messagesLocal.map(message => {
+        messages.push(message);
+        CreateMessage(message.role, message.content)
+    });
+    value_selected.value = model;
+    model_selected = model;
+}
+async function CreateMessage(role: Role, message: string) {
     const template_message = template.content.cloneNode(
         true,
     ) as HTMLElement;
@@ -152,7 +83,36 @@ async function addMessage(role: "assistant" | "user", message: string) {
     container_messages.scrollTop = container_messages.scrollHeight;
     return text;
 }
-async function createEngine(selectedModel: string) {
+async function GenerateMessageBot(engine: WebWorkerMLCEngine) {
+    let reply = "";
+    // const response = await engine.chat.completions.create({
+    //     messages,
+    //     temperature: 1,
+    //     stream: true,
+    // });
+    // const textParagraph = CreateMessage("assistant", "");
+    // for await (const chunk of response) {
+    //     const [choice] = chunk.choices;
+    //     //choice?.delta? ---->  {role:'assistand', content:''}
+    //     const content = choice?.delta?.content ?? "";
+    //     reply += content;
+    //     textParagraph.textContent = reply;
+    // }
+    // const botMessage: MessagesInterface = {
+    //     role: "assistant",
+    //     content: reply,
+    // };
+    //SIN APLICAR EL STREAM, es decir, me devuelve el contenido una vez se haya completado la 
+    //repuesta de la IA
+    // //response.choices[0].message ----> {role:'assistand', content:''}
+    // const botMessage = response.choices[0].message;
+    // if (!botMessage.content) {
+    // 	return;
+    // }
+    //CreateMessage(botMessage.role,botMessage.content);
+    //return  botMessage
+}
+async function CreateEngine(selectedModel: string) {
     const modal = document.getElementById('myModal') as HTMLDivElement;
     const info_load_engine = modal.querySelector('.content-load-engine') as HTMLSpanElement;
     if (!selectedModel) {
@@ -184,18 +144,48 @@ async function createEngine(selectedModel: string) {
     }
     return newEngine
 }
-
-async function addMessageToDb(messages: MessagesInterface[][], selectedModel: string) {
-    console.log(selectedModel)
-    try {
-        const resp = await fetch('/api/addMessage', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({ messages, selectedModel })
-        })
-    } catch (error) {
-        console.log(error)
+async function AddMessagesToChat(e: SubmitEvent) {
+    e.preventDefault();
+    errorMessage.style.display = "none";
+    errorMessage.textContent = "";
+    if (!selectedModel) {
+        alert('Por favor selecciona modelo antes de enviar un mensaje.')
+        return
     }
+    //Validando mensaje de usuario
+    let message = input.value.trim();
+    const messageValdated = validateMessage(message);
+    if (messageValdated.error) {
+        errorMessage.style.display = "block";
+        errorMessage.textContent = messageValdated.message!;
+        return;
+    }
+
+    errorMessage.style.display = "none";
+    errorMessage.textContent = "";
+
+    //Agregando mensaje de usuario
+    const userMessage: MessagesInterface = {
+        role: 'user',
+        content: message,
+    };
+    CreateMessage("user", message);
+    input.value = "";
+    button.setAttribute('disabled', 'true');
+
+    // const botMessage = await GenerateMessageBot(engine);
+    // messages.push(botMessage, userMessage);
+    messages.push(userMessage, { role: 'assistant', content: 'hola' });
+    button.removeAttribute("disabled");
+    container_messages.scrollTop = container_messages.scrollHeight;
+    const infoToSave = {
+        messages,
+        model: model_selected,
+        email: userInfo.email,
+        name: userInfo.name
+    }
+    saveMessage(infoToSave)
 }
+document.addEventListener('DOMContentLoaded', DOMloaded)
+form_select_model.addEventListener("submit", SelectModelEngine);
+form?.addEventListener("submit", AddMessagesToChat);

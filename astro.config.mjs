4e174721838a41bba32088e35380d735b0c1aef5
Juan <juanplazas28pa@gmail.com>
2024-08-06 17:15:45 -0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import auth from "auth-astro";
+import vercel from "@astrojs/vercel/serverless";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [auth(), db()],
+  output: "server",
+  adapter: vercel(),
+  image: {
+    domains: [
+      "https://lh3.googleusercontent.com/",
+    ]
+  }
+});

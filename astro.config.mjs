3ceab1a8b7e8dcf52aceb12c94bb2cd2e0da4356
Juan <juanplazas28pa@gmail.com>
2024-11-09 17:14:10 -0500

    removiendo astro:db

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth(), db()],
+  integrations: [auth()],
   output: "server",
   adapter: vercel(),
   image: {
